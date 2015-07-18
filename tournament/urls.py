47b8098c78e4f16653f1d3e5aa3b0d5fc9860948
Rafael <CoRafael@hotmail.com>
2015-07-18 21:50:21 +0100

    Home page - First Attempt

--- tournament/urls.py
+++ tournament/urls.py
@@ -8,7 +8,7 @@ from tournament import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        # url(r'^register/$', views.user_register, name='register'),
-                       # url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'base/register.html'}),
+                       url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'tournament/index.html'}),
                        # url(r'^logout/$', views.user_logout, name='logout'),
                        # url(r'^contact_us/$', views.contact_us, name='contact_us'),
                        # url(r'^about/$', views.about, name='about'),
--- tournament/views.py
+++ tournament/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     return render(request, 'tournament/index.html', {})
+
+
+# Use the login_required() decorator to ensure only those logged in can access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/')
