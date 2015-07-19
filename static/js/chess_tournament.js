aecff0ed78aa51303efdfe4c64b3a18cdd8b0099
Rafael <CoRafael@hotmail.com>
2015-07-19 18:59:42 +0100

    Home page - Added loggin and logout functionality..

--- static/js/chess_tournament.js
+++ static/js/chess_tournament.js
@@ -4,19 +4,11 @@
 
 $(document).ready(function () {
 
-
     $('#intupdatealert').hide()
-
-    $('#success').click(function (e) {
-        e.preventDefault()
-        $('#message').html('<div class="alert alert-success fade in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button>This is a success message</div>');
-    })
-
 
     $('#kostis').click(function (e) {
         e.preventDefault()
-        $('#intupdatealert').show().html('<div class="alert alert-success" role="alert" id="intupdatealert">Kostis</div>')
-
+        $('#intupdatealert').show().html('<div class="alert alert-success" role="alert" id="intupdatealert">Kostis</div>').delay(1000).fadeOut();
     })
 
 });
