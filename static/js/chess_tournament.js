55819348655b344c6fc0e13ec5141da426f1d661
Rafael <CoRafael@hotmail.com>
2015-07-21 17:00:07 +0100

    Home page - Proceed to the Tournament functionality

--- static/js/chess_tournament.js
+++ static/js/chess_tournament.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
 
     $('#intupdatealert').hide()
     $('#second_alert').hide()
-    $('#test').hide()
 
     $('#refresh_button').click(function (e) {
         e.preventDefault()
@@ -24,11 +23,24 @@ $(document).ready(function () {
         $('#inputEloRating').prop("readonly", true);
         $('#register_button').remove()
         $('#upload_file').remove()
-        var toAddEnter = '<div class="alert alert-warning" role="alert">Sorry but the tournament has started. No more chess players can be added </div>';
-        var toAddUpload = '<div class="alert alert-warning" role="alert">Yes, no .csv files are allowed as well. Observe the leaderboard. Feel the vibe  </div>';
-        $('#create_chess_player').append(toAddEnter)
-        $('#upload_multiple').append(toAddUpload)
+        if (!$('#add_enter').length) {
+            var toAddEnter = '<div class="alert alert-warning" role="alert" id="add_enter">Sorry but the tournament has started. No more chess players can be added </div>';
+            $('#create_chess_player').append(toAddEnter)
+        }
+        if (!$('#add_upload').length) {
+            var toAddUpload = '<div class="alert alert-warning" role="alert" id="add_upload">Yes, no .csv files are allowed as well. Observe the leaderboard. Feel the vibe  </div>';
+            $('#upload_multiple').append(toAddUpload)
+        }
+        $('#preliminary_text').fadeOut()
 
+
+        var getData = getLeaderBoard()
+
+        //var item = $('<div class="alert alert-warning" role="alert" id="add_enter">Sorry but the tournament has started. No more chess players can be added </div>').hide().fadeIn(2000);
+        //$('#start_tournament').append(getData);
+        //$('html, body').animate({
+        //    scrollTop: $("#enter_participants").offset().top
+        //}, 1000);
 
     })
 
@@ -61,30 +73,84 @@ function print_second_alert_message(message) {
 }
 
 
+function getLeaderBoardRowsToAdd(message) {
+    var json = jQuery.parseJSON(message);
+    var test = json['chess_players']
+    var all_players = $.parseJSON(test);//parse
+    var content = '';
+    content += '<tbody>';
+    for (var i in all_players) {
+        content += '<tr' + '">';
+        var fields = all_players[i].fields
+        var index = parseInt(i, 10) + 1;
+        content += '<td>' + index + '</td>';
+        content += '<td>' + fields.name + '</td>';
+        content += '<td>' + fields.surname + '</td>';
+        content += '<td>' + fields.country + '</td>';
+        content += '<td>' + fields.elo_rating + '</td>';
+        content += '</tr>';
+    }
+    content += '</tbody>';
+    return content
+}
+
+
+function getLeaderBoard() {
+    var result = "Something went wrong. Please retry";
+    $.ajax({
+            type: 'GET',
+            url: '/update_table/',
+            dataType: 'text',
+            async: false,
+            success: function (message) {
+                var rowsToAdd = getLeaderBoardRowsToAdd(message)
+                var initTable = ''
+                initTable += '<div class=\"container\">'
+                initTable += '<div class=\"panel-body panel-refresh\">'
+                initTable += '<table class=\"table table-striped\" id=\"leaderboard_to_add\">'
+                initTable += '<thead>'
+                initTable += '<tr>'
+                initTable += '<th> Rank </th>'
+                initTable += '<th> Name </th>'
+                initTable += '<th> Surname </th>'
+                initTable += '<th> Country </th>'
+                initTable += '<th> Score </tr>'
+                initTable += '</tr>'
+                initTable += '</thead>'
+                initTable += '<tbody>'
+                initTable += rowsToAdd
+                initTable += '</tbody>'
+                initTable += '</div>'
+                initTable += '</div>'
+                var kokos = '<div class=\"container\">'
+                kokos += '<div class="row col-lg-12 text-center" >'
+                kokos += '<h4>These are the chess participants that are competing in our tournament... Move forward? </h4>'
+                kokos += '<button type=\"submit\" class=\"btn btn-primary login_logout\" id=\"yeah_let_start\">Yeah, Let\'s start! </button>'
+                kokos += '</div>'
+                kokos += '</div>'
+
+
+                $('#start_tournament_inner').html(initTable);
+                $('#start_tournament_inner').append(kokos);
+
+                $('html, body').animate({
+                    scrollTop: $("#yeah_let_start").offset().top
+                }, 1000);
+            }
+        }
+    )
+    ;
+    return result;
+}
+
+
 function refresh() {
     $.ajax({
         type: 'GET',
         url: '/update_table/',
         dataType: 'text',
         success: function (message) {
-            var json = jQuery.parseJSON(message);
-            var test = json['chess_players']
-            var all_players = $.parseJSON(test);//parse
-            var content = '';
-            content += '<tbody>';
-            for (var i in all_players) {
-                content += '<tr' + '">';
-                var fields = all_players[i].fields
-                var index = parseInt(i, 10) + 1;
-                content += '<td>' + index + '</td>';
-                content += '<td>' + fields.name + '</td>';
-                content += '<td>' + fields.surname + '</td>';
-                content += '<td>' + fields.country + '</td>';
-                content += '<td>' + fields.elo_rating + '</td>';
-                content += '</tr>';
-            }
-            content += '</tbody>';
-            $('#table_results tbody').replaceWith(content);
+            $('#table_results tbody').replaceWith(getLeaderBoardRowsToAdd(message));
             print_alert_message("The League has been updated")
         }
     });
